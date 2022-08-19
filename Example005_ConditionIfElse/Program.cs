050cef5a1ed013797e51b33da01da4c50129f919
Diana Bayazitova <onlydi@mail.ru>
2022-08-19 21:54:08 +0500

    Added folder Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input username");
+string username=Console.ReadLine();
+if (username.ToLower=="masha")
+{
+    Console.WriteLine ("Ura, it's Masha!");
+
+}
+else {
+    Console.Write ("Hello, ");
+    Console.WriteLine (username);
+}
